29b6d17f494c4ba1aa5d6ed2e4e8a6a1e741b1f9
Dmitry Rets <dmitryrets@gmail.com>
2017-05-21 14:58:51 +0200

    Add estimated reading time.

--- src/site.hs
+++ src/site.hs
@@ -2,23 +2,25 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import System.FilePath (takeFileName, (-<.>), (</>))
-import Data.Monoid ((<>))
-import Data.Maybe (fromMaybe, listToMaybe)
+import           Control.Exception
 import qualified Control.Monad.State as St
-import Data.Either (either)
-import Data.List (sortBy)
-import Data.Function ((&))
-import Data.Ord (comparing)
-import System.IO
-import Control.Exception
-import Data.String (fromString)
-import Text.Read (readMaybe)
-import Hakyll
-import Text.Highlighting.Kate (styleToCss, tango)
-import Text.Pandoc.Options
-import qualified Github.Issues as G
+import           Data.Either (either)
+import           Data.Function ((&))
+import           Data.List (sortBy)
+import           Data.Maybe (fromMaybe, listToMaybe)
+import           Data.Monoid ((<>))
+import           Data.Ord (comparing)
+import           Data.String (fromString)
 import qualified Github.Auth as G
+import qualified Github.Issues as G
+import           Hakyll
+import           System.FilePath (takeFileName, (-<.>), (</>))
+import           System.IO
+import           Text.Highlighting.Kate (styleToCss, tango)
+import           Text.Pandoc.Definition
+import           Text.Pandoc.Options
+import           Text.Pandoc.Options
+import           Text.Read (readMaybe)
 
 import qualified Scripts as Scr
 
@@ -52,13 +54,88 @@ processWithPandoc' withToc =
     renderPandocWith defaultHakyllReaderOptions
         (if withToc then tocPandocWriterOptions else ourPandocWriterOptions)
 
-pandocCompilerOfOurs :: Compiler (Item String)
-pandocCompilerOfOurs = pandocCompilerOfOurs' False
+pandocCompilerOfOursWithTransform :: Compiler (Item String)
+pandocCompilerOfOursWithTransform = do
+  pandocCompilerWithTransform
+    defaultHakyllReaderOptions
+    ourPandocWriterOptions
+    ourTransform
 
-pandocCompilerOfOurs' :: Bool -> Compiler (Item String)
-pandocCompilerOfOurs' withToc =
-    pandocCompilerWith defaultHakyllReaderOptions $
-        if withToc then tocPandocWriterOptions else ourPandocWriterOptions
+pandocCompilerOfOurs :: Compiler (Item String)
+pandocCompilerOfOurs = do
+  pandocCompilerWith
+    defaultHakyllReaderOptions
+    ourPandocWriterOptions
+
+ourTransform :: Pandoc -> Pandoc
+ourTransform p@(Pandoc meta blocks) = (Pandoc meta (ert:blocks))
+  where
+    ert = Para [ Emph [ Str $ "(", Str $ timeEstimateString p, Str $ " read)"] ]
+
+timeEstimateString :: Pandoc -> String
+timeEstimateString = toClockString . timeEstimateSeconds
+
+toClockString :: Int -> String
+toClockString i
+    | i >= 60 * 60 = show hours ++ "h" ++ show minutes ++ "m" ++ show seconds ++ "s"
+    | i >= 60      = show minutes ++ "m" ++ show seconds ++ "s"
+    | otherwise    = show seconds ++ "s"
+  where
+    hours   = i `quot` (60*60)
+    minutes = (i `rem` (60*60)) `quot` 60
+    seconds = i `rem` 60
+
+timeEstimateSeconds :: Pandoc -> Int
+timeEstimateSeconds = (`quot` 5) . nrWords
+
+nrWords :: Pandoc -> Int
+nrWords = (`quot` 5) . nrLetters
+
+nrLetters :: Pandoc -> Int
+nrLetters (Pandoc _ bs) = sum $ map cb bs
+  where
+    cbs = sum . map cb
+    cbss = sum . map cbs
+    cbsss = sum . map cbss
+
+    cb :: Block -> Int
+    cb (Plain is) = cis is
+    cb (Para is) = cis is
+    cb (CodeBlock _ s) = length s
+    cb (RawBlock _ s) = length s
+    cb (BlockQuote bs) = cbs bs
+    cb (OrderedList _ bss) = cbss bss
+    cb (BulletList bss) = cbss bss
+    cb (DefinitionList ls) = sum $ map (\(is, bss) -> cis is + cbss bss) ls
+    cb (Header _ _ is) = cis is
+    cb HorizontalRule = 0
+    cb (Table is _ _ tc tcs) = cis is + cbss tc + cbsss tcs
+    cb (Div _ bs) = cbs bs
+    cb Null = 0
+
+    cis = sum . map ci
+    ciss = sum . map cis
+
+    ci :: Inline -> Int
+    ci (Str s) = length s
+    ci (Emph is) = cis is
+    ci (Strong is) = cis is
+    ci (Strikeout is) = cis is
+    ci (Superscript is) = cis is
+    ci (Subscript is) = cis is
+    ci (SmallCaps is) = cis is
+    ci (Quoted _ is) = cis is
+    ci (Cite _ is) = cis is
+    ci (Code _ s) = length s
+    ci Space = 1
+    ci SoftBreak = 1
+    ci LineBreak = 1
+    ci (Math _ s) = length s
+    ci (RawInline _ s) = length s
+    ci (Link _ is (_, s)) = cis is + length s
+    ci (Image _ is (_, s)) = cis is + length s
+    ci (Note bs) = cbs bs
+
 
 rssConfig :: FeedConfiguration
 rssConfig = FeedConfiguration
@@ -181,7 +258,7 @@ theSite = do
     match allPosts $ do
         route   $ setExtension "html"
         compile $ do
-            pandocCompilerOfOurs
+            pandocCompilerOfOursWithTransform
                 >>= saveSnapshot "content"
                 >>= loadAndApplyTemplate
                     "templates/post.html" postCtx
